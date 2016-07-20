1460fa2ddb7ecb0fe09f1f20addaed76ae7785c1
jntp <justin.jntprules@gmail.com>
2016-07-19 21:13:33 -0700

    More 'parameter' shit >:D

--- main.cpp
+++ main.cpp
@@ -2,25 +2,28 @@
 #include <string> 
 
 struct parameters {
-	int iCity; // Stores the indicated city/region 
-
+	int iCity, // Stores the indicated city/region
+	    iStorm, // Type of storm
+	    iWeather; // Type of weather
+	bool bWind;  // True for windy weather events 
 } forecast;
 
 /*
- * Checks input vaiable to see if it falls within the correct parameters. If not, "switches" the bool variable and returns it. 
+ * Checks input variable to see if it falls within the correct parameters. Returns a bool based on the response to the condition.  
  * Input: int variable to be checked, lower range, upper range, bool variable 
- * Output: bool variable "switched"
+ * Output: bool variable in response to the condition 
  */
-bool checkInput(int iKey, int iLower, int iUpper, bool bSwitch);
+bool CheckInput(int iKey, int iLower, int iUpper, bool bBounds);
 
 int main() {
 	using namespace std;
 	
 	// Fill in parameters
-	bool bLoop = false; 
+	bool bLoop = true; // Indicates whether do-while loop should repeat
 	
 	// City/Region 
 	do {
+		// List options for user to choose
 		cout << "Choose your city/region: " << endl;
 		cout << "0: San Francisco/Oakland, CA" << endl;
 		cout << "1: Davis/Sacramento, CA" << endl;
@@ -30,22 +33,72 @@ int main() {
 		bLoop = CheckInput(forecast.iCity, 0, 3, bLoop); // Check for correct input
 	} while (!bLoop);
 
+	// Storm Type
+	do {
+		cout << "Choose the storm type: " << endl; 
+		cout << "0: Weak Storm" << endl;
+		cout << "1: Moderate Storm" << endl;
+		cout << "2: Strong Storm" << endl;
+		cout << "3: Tropical Remnants" << endl;
+		cout << "4: Tropical Storm" << endl; 
+		cin >> forecast.iStorm; 
+		bLoop = CheckInput(forecast.iStorm, 0, 4, bLoop); // Check for correct input 
+	} while (!bLoop);
+
+	// Weather Type
+	int iWind; // Temporary store as int variable before converting to bool
+	bool bLoopTwo; // Checks if nested while loop should repeat	
+
+	do {
+		bLoopTwo = true; // By default while loop below should not run
+		
+		cout << "Choose the weather type: " << endl;
+		cout << "0: Light Rain" << endl;
+		cout << "1: Moderate Rain" << endl;
+		cout << "2: Heavy Rain" << endl;
+		cout << "3: Thunderstorms" << endl;
+		cout << "4: Fog/Drizzle" << endl;
+		cout << "5: Snow Showers" << endl;
+		cout << "6: Rain mixed with Snow" << endl; 
+		cin >> forecast.iWeather; 
+		bLoop = CheckInput(forecast.iWeather, 0, 6, bLoop); // Check for correct input
+	
+		if (bLoop) { // If the above input is correct then run the while loop 
+			bLoopTwo = false; // Set to false so that while loop below can run 
+		}
+
+		// Confirm to user if strong winds exist in this event	
+		while (!bLoopTwo) { // Repeat if user fails to give proper answer
+			cout << "Are there strong winds with this event?" << endl;
+			cout << "0: no" << endl;
+			cout << "1: yes" << endl; 	
+			cin >> iWind;
+			bLoopTwo = CheckInput(iWind, 0, 1, bLoopTwo); // Check for correct input 
+			if (bLoopTwo) { // Check for correct input before assigning value for bWin
+				forecast.bWind = iWind; // Assign the iWind input to the bool bWind	
+			}
+		}
+	} while (!bLoop);	
+
 	return 0;
 }
 
 /*
- * Checks input vaiable to see if it falls within the correct parameters. If not, "switches" the bool variable and returns it. 
+ * Checks input variable to see if it falls within the correct parameters. Returns a bool based on the response to the condition.  
  * Input: int variable to be checked, lower range, upper range, bool variable 
- * Output: bool variable "switched"
+ * Output: bool variable in response to the condition 
  */
-bool CheckInput(int iKey, int iLower, int iUpper, bool bSwitch) {
+bool CheckInput(int iKey, int iLower, int iUpper, bool bBounds) {
 	// Check if iKey is within iLower and iUpper
 	if (iKey >= iLower && iKey <= iUpper)  { // Within in the range
-		return bSwitch; // Simply return the bool variable
+		std::cout << std::endl; 
+		bBounds = true; 
+		return bBounds; // Return the bool variable
 	} else { // Out of bounds
-		std::cout << "Error! Please input a number between " iLower " and " iUpper "." << std::endl; 
-		bSwitch = !bSwitch // "Switch" the bool variable
-		return bSwitch; // Return switched bool variable
+		std::cout << "Error! Please input a number between " <<  iLower << " and " << iUpper << "." << std::endl; 
+		std::cout << std::endl; 
+		bBounds = false;  
+		return bBounds; 
 	}
 }
 
