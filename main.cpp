513a225498eeb091118d095aed3cdd0fe80e394b
jntp <justin.jntprules@gmail.com>
2016-09-27 18:56:49 -0700

    modified main.cpp to add Twilio Rest API code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include "parameters.h"
 
+using namespace std;
+
+#include "Utils.h"
+#include "Rest.h"
+#include "TwiML.h"
+
+using namespace twilio;
 
 int main() {
-	using namespace std;
-
 	string sForecast = Parameters(); // Run the "Fill in Parameters" part of the code and obtain weather forecast
 
 	// Display the string
@@ -33,6 +39,37 @@ int main() {
 	} else { // If file failed to open succesfully
 		cout << "Error! Unable to open file." << endl; // Display error message 
 	}
+
+	// Give user time to edit forecast.txt
+	string sInput = ""; // Allow for user input using getline
+	
+	cout << endl;
+	cout << "Please take the time to make any last minute changes to the file." << endl;
+	cout << "When you're done, press any key to continue." << endl; 
+	getline(cin, sInput);
+
+
+	/****** HERE MARKS THE BEGINNING OF TWILIO REST API CODE ******/
+
+	// Twilio REST API version
+	const string API_version = "2010-04-01"; 
+	
+	// Twilio AccountSid and AuthToken
+	const string ACCOUNT_SID = "XXXX";
+	const string ACCOUNT_TOKEN = "XXXX";
+	
+	// Outgoing Caller ID previous validated with Twilio
+	const string CALLER_ID = "XXXX";
+
+	string response;
+	
+	vector<Var> vars;
+
+	
+
+		
+
+	
 	
 	return 0;
 }
